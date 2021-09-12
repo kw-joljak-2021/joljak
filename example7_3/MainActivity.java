8d18192b4bfe3414cb4dd1b50918b45493e09cd0
subinze <beenjune@naver.com>
2021-09-12 14:53:33 +0900

    Create MainActivity.java

---
+++ example7_3/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.example7_3;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvName, tvEmail, toastText;
+    Button button1;
+    EditText dlgEdtName, dlgEdtEmail;
+    View dialogView, toastView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("사용자 정보 입력");
+
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogView = (View) View.inflate(MainActivity.this,
+                        R.layout.dialog1, null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("사용자 정보 입력");
+                dlg.setView(dialogView);
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+                        dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2);
+
+                        tvName.setText(dlgEdtName.getText().toString());
+                        tvEmail.setText(dlgEdtEmail.getText().toString());
+                    }
+                });
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        Toast toast = new Toast(MainActivity.this);
+                        toastView = (View) View.inflate(MainActivity.this,
+                                R.layout.toast1, null);
+                        toastText = (TextView) toastView.findViewById(R.id.toastText1);
+                        toastText.setText("취소했습니다");
+                        toast.setView(toastView);
+                        toast.show();
+                    }
+                });
+                dlg.show();
+            }
+        });
+    }
+}
