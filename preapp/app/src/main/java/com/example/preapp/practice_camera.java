297824a501ec78cfba793575d225109ebeacbd98
subinze <beenjune@naver.com>
2021-10-02 00:24:22 +0900

    update consonant, vowel

---
+++ preapp/app/src/main/java/com/example/preapp/practice_camera.java
@@ -0,0 +1,17 @@
+package com.example.preapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class practice_camera extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.practice_camera);
+        setTitle("지문자 연습");
+
+        //아이콘 xml에서 사진 추가
+        // 카메라 java에서 코드 짜서 진행 후 xml 코드 지우기
+        // 카메라 밑에 통과 여부 표시 카메라 설치 후 진행
+    }
+}
---
+++ preapp/app/src/main/java/com/example/preapp/vowel.java
@@ -0,0 +1,14 @@
+package com.example.preapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class vowel extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vowel);
+        setTitle("모음");
+    }
+}
