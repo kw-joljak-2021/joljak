0fdb0ae2641afa05542d8aa544832edb24a67f53
ringring2 <39092926+ringring2@users.noreply.github.com>
2021-10-01 23:27:14 +0900

    aa

--- preapp/app/src/main/java/com/example/preapp/practice1.java
+++ preapp/app/src/main/java/com/example/preapp/practice1.java
@@ -1,6 +1,7 @@
 package com.example.preapp;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,7 @@ public class practice1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.practice1);
+        ProgressBar bar1 = (ProgressBar) findViewById(R.id.bar1);
+        ProgressBar bar2 = (ProgressBar) findViewById(R.id.bar2);
     }
 }
